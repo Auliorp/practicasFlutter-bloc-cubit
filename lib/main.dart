37ee3bdda58ba8f17205121b02992270410f919e
Aulio Rovero <aulioroveroparedes@gmail.com>
2024-03-14 10:23:15 -0300

    Se trabajo el estado del tema de la app, y se manipulo cubit de cambio de nombre random

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,12 @@ class BlocsProviders extends StatelessWidget {
         ),
         BlocProvider(
           create: (context) => RouterSimpleCubit(),
+        ),
+        BlocProvider(
+          create: (context) => CounterCubit(),
+        ),
+        BlocProvider(
+          create: (context) => ThemeCubit(),
         )
       ],
       child: const MyApp(),
@@ -34,12 +40,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     //
     final appRouter = context.watch<RouterSimpleCubit>().state;
+    final theme = context.watch<ThemeCubit>().state;
     //
     return MaterialApp.router(
       title: 'Flutter BLoC',
       debugShowCheckedModeBanner: false,
       routerConfig: appRouter,
-      theme: AppTheme(isDarkmode: false).getTheme(),
+      theme: AppTheme(isDarkmode: theme.isDarkmode).getTheme(),
     );
   }
 }
